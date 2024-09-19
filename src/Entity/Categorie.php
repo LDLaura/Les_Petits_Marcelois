d2af893634d65a6b0548aaf344685b1f74cf1c7d
LDLaura <despesselaura@yahoo.fr>
2024-09-19 10:19:08 +0200

    feat(categorie): make entity categorie

---
+++ src/Entity/Categorie.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\DateTimeTrait;
+use App\Entity\Traits\EnableTrait;
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: CategorieRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class Categorie
+{
+    use DateTimeTrait, EnableTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 255)]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne(inversedBy: 'categories')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?user $user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getUser(): ?user
+    {
+        return $this->user;
+    }
+
+    public function setUser(?user $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
