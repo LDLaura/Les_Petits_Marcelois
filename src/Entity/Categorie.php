a4f3015bc7a4ee21e8c8004f1f0fb3b523a79350
LDLaura <despesselaura@yahoo.fr>
2024-09-19 15:42:48 +0200

    feat(categorie): crud

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -25,10 +25,6 @@ class Categorie
     #[Assert\Length(max: 255)]
     private ?string $name = null;
 
-    #[ORM\ManyToOne(inversedBy: 'categories')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?user $user = null;
-
     public function getId(): ?int
     {
         return $this->id;
@@ -42,18 +38,6 @@ class Categorie
     public function setName(string $name): static
     {
         $this->name = $name;
-
-        return $this;
-    }
-
-    public function getUser(): ?user
-    {
-        return $this->user;
-    }
-
-    public function setUser(?user $user): static
-    {
-        $this->user = $user;
 
         return $this;
     }
